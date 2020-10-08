f00341050c36e9908d3729f8ffb594f651466cf7
Simona <see.moona@yahoo.com>
2020-10-08 23:15:03 +0200

    animation working

---
+++ client/src/components/UI/Button.js
@@ -0,0 +1,20 @@
+// import React from 'react';
+
+// import { Transition, CSSTransition } from 'react-transition-group';
+
+// const Button = ({ in: inProp }) => {
+//   return (
+//     <CSSTransition
+//       unmountOnExit
+//       in={inProp}
+//       timeout={{ appear: 0, enter: 0, exit: 300 }}
+//       classNames="roll"
+//       appear
+//       exit
+//     >
+//       <button>Are you new here?</button>
+//     </CSSTransition>
+//   );
+// };
+
+// export default Button;
