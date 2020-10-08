b18a8681e48f03e99cec363ffdca5f5f6e7dbfc7
Simona <see.moona@yahoo.com>
2020-10-08 23:16:36 +0200

    component routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './scss/main.scss';
 import Auth from './components/Auth/Auth';
 import Login from './components/Login/Login';
+import Home from './components/Home/Home';
 
-function App() {
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+const App = () => {
   return (
-    <div className="App">
-      <Auth />
-      <Login />
-    </div>
+    <Router>
+      <div className="App">
+        <Route path="/" exact component={Home}></Route>
+        <Route path="/register" component={Auth}></Route>
+        <Route path="/login" component={Login}></Route>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
