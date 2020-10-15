152a40497e26507acaaedcc046bca63b4e95980a
Simona <see.moona@yahoo.com>
2020-10-15 14:44:50 +0200

    new update

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const path = require('path');
 app.use(morgan('dev'));
 require('dotenv').config();
 // to process the json data
@@ -10,22 +11,35 @@ app.use(bodyParser.json());
 app.use(bodyParser.raw());
 // mongoDB
 const mongoose = require('mongoose');
+
 mongoose
-  .connect(process.env.DB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-  })
+  .connect(
+    'mongodb+srv://simo:NzDv9m1UacOtHKhs@cluster0.wx3du.mongodb.net/authentication?retryWrites=true&w=majority',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    }
+  )
   .then(console.log('DB is connected'))
   .catch((error) => {
     console.log(`There was a problem ${error.message}`);
   });
 
 app.use(express.json());
+// Serving Static Files
+app.use(express.static(path.join(__dirname, './client/build')));
 
+//Routes
 const userControllers = require('./controllers/userControllers');
 app
   .get('/register', userControllers.getAllUsers)
   .post('/register', userControllers.registerUser);
 app.post('/login', userControllers.login);
+
+//send react application
+app.get('/', function (req, res) {
+  const index = path.join(__dirname, './client/build', 'index.html');
+  res.sendFile(index);
+});
 module.exports = app;
---
+++ client/build/precache-manifest.29d83fea529c91527a91991d545d17b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d15b9dcb2bb14fdb812a8d5983ad9ef5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e433ff35250de34b78c3",
+    "url": "/static/css/main.dcf0bdb3.chunk.css"
+  },
+  {
+    "revision": "8dbff8db5c1234213fe6",
+    "url": "/static/js/2.8428338b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8428338b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e433ff35250de34b78c3",
+    "url": "/static/js/main.6002bde0.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
